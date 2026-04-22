c63a308aeb2b8e96f1f754d27fc2d3013bac49e5
malakkhaled22 <malakkhaled178@gmail.com>
2026-04-22 12:11:30 +0200

    Refactor node mailer

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -6,7 +6,7 @@ export const sendEmail = async (to: string, subject: string, text: string) => {
         console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
         const transporter = nodemailer.createTransport({
             host: "smtp.gmail.com",
-            port: 465,
+            port: 587,
             secure: true,
             auth: {
                 user: process.env.EMAIL_USER,
