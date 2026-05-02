d0b411a4b62a4ebe39f7e71e97ba264d444dba83
malakkhaled22 <malakkhaled178@gmail.com>
2026-05-02 18:07:04 +0300

    Refactor roles APIs with new tokens

--- src/controllers/roles.controller.ts
+++ src/controllers/roles.controller.ts
@@ -1,39 +1,48 @@
 import { Request, Response } from "express"; 
 import { User } from "../models/user.model";
-
-
+import { generateToken } from "../utils/generateToken";
 
 export const chooseRole = async (req: Request, res: Response) => {
     try {
         const userId = (req as any).user._id;
         const { role } = req.body;
 
-        if (!role) {
-            return res.status(400).json({ message: "Role is required" });
-        }
-
+        if (!role) return res.status(400).json({ message: "Role is required" });
         const newRole = role.toLowerCase();
 
-        if (!["buyer", "seller"].includes(newRole)) {
-            return res.status(400).json({ message: "Invalid role" });
-        }
+        if (!["buyer", "seller"].includes(newRole)) return res.status(400).json({ message: "Invalid role" });
 
         const user = await User.findById(userId);
-        if (!user) {
-            return res.status(404).json({ message: "User not found" });
-        }
-
-        if (user.isBuyer || user.isSeller) {
+        if (!user) return res.status(404).json({ message: "User not found" });
+        if (user.isBuyer || user.isSeller) 
             return res.status(400).json({ message: "Role already selected!, use switch instead." });
-        }
 
         user.isBuyer = newRole === "buyer";
         user.isSeller = newRole === "seller";
 
+        const { accessToken, refreshToken } = generateToken({
+            _id: userId.toString(),
+            isSeller: user.isSeller,
+            isBuyer: user.isBuyer,
+            isAdmin: user.isAdmin,
+        });
+        user.refreshToken =refreshToken;
         await user.save();
 
+        const safeUser = {
+            id: userId,
+            username: user.username,
+            email: user.email,
+            phone: user.phone,
+            isBuyer: user.isBuyer,
+            isSeller: user.isSeller
+        };
+
         return res.status(200).json({
-            message: `Role successfully chosen: ${newRole}`
+            message: `Role successfully chosen: ${newRole}`,
+            accessToken,
+            refreshToken,
+            safeUser,
         });
 
     } catch (error) {
@@ -44,46 +53,51 @@ export const chooseRole = async (req: Request, res: Response) => {
 
 export const switchRole = async (req: Request, res: Response) => {
     try {
-        const userId = (req as any).user._id; // from token
+        const userId = (req as any).user._id;
         const { newRole } = req.body;
 
-        if (!newRole) {
-            return res.status(400).json({ message: "New role is required" });
-        }
-
+        if (!newRole) return res.status(400).json({ message: "New role is required" });
         const role = newRole.toLowerCase();
 
-        if (!["buyer", "seller"].includes(role)) {
-            return res.status(400).json({ message: "Invalid role!" });
-        }
+        if (!["buyer", "seller"].includes(role)) return res.status(400).json({ message: "Invalid role!" });
 
         const user = await User.findById(userId);
-        if (!user) {
-            return res.status(404).json({ message: "User not found!" });
-        }
 
-        if (!user.canSwitchRole) {
-            return res.status(403).json({ message: "You cannot switch roles" });
-        }
-        
-        if (!user.isBuyer && !user.isSeller) {
-            return res.status(400).json({ message: "No role selected yet, use chooseRole first" });
-        }
-
-        if ((role === "buyer" && user.isBuyer) || (role === "seller" && user.isSeller)) {
+        if (!user) return res.status(404).json({ message: "User not found!" }); 
+        if (!user.canSwitchRole) return res.status(403).json({ message: "You cannot switch roles" });
+        if (!user.isBuyer && !user.isSeller) return res.status(400).json({ message: "No role selected yet, use chooseRole first" });
+        if ((role === "buyer" && user.isBuyer) || (role === "seller" && user.isSeller)) 
             return res.status(400).json({ message: `You are already a ${role}` });
-        }
 
         user.isBuyer = role === "buyer";
         user.isSeller = role === "seller";
 
+        const { accessToken, refreshToken } = generateToken({
+            _id: userId.toString(),
+            isSeller: user.isSeller,
+            isBuyer: user.isBuyer,
+            isAdmin: user.isAdmin,
+        });
+        user.refreshToken = refreshToken;
         await user.save();
+
+        const safeUser = {
+            id: userId,
+            username: user.username,
+            email: user.email,
+            phone: user.phone,
+            isBuyer: user.isBuyer,
+            isSeller: user.isSeller
+        };
 
         res.status(200).json({
             message: `Role switched to ${role} successfully`,
-            user,
+            accessToken,
+            refreshToken,
+            safeUser,
         });
     } catch (error) {
-        res.status(500).json({ message: "Server Error", error });
+        console.error("Error in Switch Role: ", error);
+        res.status(500).json({ message: "Internal Server Error" });
     }
 };
